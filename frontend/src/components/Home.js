d19ce1a3ba4c4cd7166aaaa5bbf4b12258352f44
jenna-anderson <jenna.d.anderson@gmail.com>
2021-07-29 16:23:52 -0600

    test http request successfully talks to deployed backend

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios'
+
 
 const Home = () => {
+    useEffect(() => {
+        axios.get('https://did-you-feed-the-dog.herokuapp.com/api')
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
+    })
     return(
         <div>
             <h1>Did you feed the dog?</h1>
