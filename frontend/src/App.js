6be2a056b3f8f5e205086e90559471f6d5c57724
jenna-anderson <jenna.d.anderson@gmail.com>
2021-07-29 14:35:34 -0600

    creates Navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import React from 'react'
 
 import Home from './components/Home'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <header className="App-header">
         <Home/>
       </header>
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return(
+        <nav>
+            <a>Home</a>
+            <a>Login</a>
+            <a>Signup</a>
+        </nav>
+    )
+}
+
+export default Navbar
