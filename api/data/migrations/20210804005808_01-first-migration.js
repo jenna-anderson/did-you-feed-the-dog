b72a73b752c55a94f928636c48dd77037067758d
jenna-anderson <jenna.d.anderson@gmail.com>
2021-08-09 16:44:15 -0600

    adds users_dogs table to migration

--- api/data/migrations/20210804005808_01-first-migration.js
+++ api/data/migrations/20210804005808_01-first-migration.js
@@ -2,7 +2,7 @@
 exports.up = async function(knex) {
     await knex.schema
     .createTable('users', (users) => {
-        users.increments()
+        users.increments('user_id')
         users.string('email', 200).notNullable().unique()
         users.string('password', 200).notNullable()
     })
@@ -33,9 +33,25 @@ exports.up = async function(knex) {
         .onDelete('CASCADE')
         .onUpdate('CASCADE')
     })
+    .createTable('users_dogs', (users_dogs) => {
+      users_dogs.increments('user_dog_id')
+      users_dogs.integer('user_id')
+        .unsigned()
+        .references('user_id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+      users_dogs.integer('dog_id')
+        .unsigned()
+        .references('dog_id')
+        .inTable('dogs')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE')
+    })
   };
   
   exports.down = async function(knex) {
+    await knex.schema.dropTableIfExists('users_dogs')
     await knex.schema.dropTableIfExists('feedings')
     await knex.schema.dropTableIfExists('weights')
     await knex.schema.dropTableIfExists('dogs')
